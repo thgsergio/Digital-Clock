4d0f4b243ad51992ec7e52b10a8b4c0938d24762
Thiago Sergio <87444593+thgsergio@users.noreply.github.com>
2025-09-08 15:31:46 -0300

    fix: Alteração de nomes de variáveis

--- maq_m/maq_m.sv
+++ maq_m/maq_m.sv
@@ -3,8 +3,8 @@ module maq_m(
 	input maqm_reset,
 	input maqm_enable,
 	input maqm_incremento,
-	output logic [3:0] maqm_lsd,
-	output logic [2:0] maqm_msd,
+	output logic [3:0] maqm_Lsd,
+	output logic [2:0] maqm_Msd,
 	output logic maqm_incrementahora
 );
 
@@ -12,25 +12,25 @@ always_ff @(posedge maqm_clock, negedge maqm_reset)
 begin
 	if(!maqm_reset)
 	begin
-		maqm_lsd <= 4'b0000;
-		maqm_msd <= 3'b000;
+		maqm_Lsd <= 4'b0000;
+		maqm_Msd <= 3'b000;
 		maqm_incrementahora <= 1'b0;
 	end
 	else if(maqm_enable && maqm_incremento)
 	begin
-		if((maqm_lsd == 4'b0000) && (maqm_msd == 3'b000))
+		if((maqm_Lsd == 4'b0000) && (maqm_Msd == 3'b000))
 			maqm_incrementahora <= 1'b0;
 		
-		maqm_lsd <= maqm_lsd + 1'b1;
+		maqm_Lsd <= maqm_Lsd + 1'b1;
 		
-		if(maqm_lsd == 4'b1010) 
+		if(maqm_Lsd == 4'b1010) 
 		begin
-			maqm_lsd <= 4'b0000;
-			maqm_msd <= maqm_msd + 1'b1;
+			maqm_Lsd <= 4'b0000;
+			maqm_Msd <= maqm_Msd + 1'b1;
 				
-			if(maqm_msd == 3'b110) 
+			if(maqm_Msd == 3'b110) 
 			begin
-				maqm_msd <= 3'b000;
+				maqm_Msd <= 3'b000;
 				maqm_incrementahora <= 1'b1;
 			end
 		end	 
