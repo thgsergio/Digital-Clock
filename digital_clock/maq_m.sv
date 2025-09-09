3df9d9e5ede618215d852afdf0d4951a11ba8066
LuisEd83 <eduluis784@gmail.com>
2025-09-08 21:08:04 -0300

    fix maq_m.sv

--- digital_clock/maq_m.sv
+++ digital_clock/maq_m.sv
@@ -8,34 +8,35 @@ module maq_m(
 	output logic maqm_incrementahora
 );
 
-always_ff @(posedge maqm_clock, negedge maqm_reset)
-begin
+always_ff @(posedge maqm_clock or negedge maqm_reset) begin
 	if(!maqm_reset)
-	begin
-		maqm_Lsd <= 4'b0000;
-		maqm_Msd <= 3'b000;
-		maqm_incrementahora <= 1'b0;
-	end
-	else if(maqm_enable && maqm_incremento)
-	begin
-		if((maqm_Lsd == 4'b0000) && (maqm_Msd == 3'b000))
-			maqm_incrementahora <= 1'b0;
-		
-		maqm_Lsd <= maqm_Lsd + 1'b1;
-		
-		if(maqm_Lsd == 4'b1010) 
 		begin
-			maqm_Lsd <= 4'b0000;
-			maqm_Msd <= maqm_Msd + 1'b1;
-				
-			if(maqm_Msd == 3'b110) 
+		maqm_Lsd <= 4'b0000;
+      maqm_Msd <= 3'b000;
+      maqm_incrementahora <= 1'b0;
+		end
+	else if((maqm_enable) && (maqm_incremento)) 
+		begin
+			
+		if(maqm_Lsd == 4'b1001) 
 			begin
-				maqm_Msd <= 3'b000;
-				maqm_incrementahora <= 1'b1;
-			end
-		end	 
-	end
-	
-end
+			maqm_Lsd <= 4'b0000;
+
+			if(maqm_Msd == 3'b101) 
+				begin
+					maqm_Msd <= 3'b000;
+					maqm_incrementahora <= 1'b1;
+				end else 
+					begin
+					maqm_Msd <= maqm_Msd + 1'b1;
+					maqm_incrementahora <= 1'b0;
+					end
+				end
+			end else 
+				begin
+				maqm_Lsd <= maqm_Lsd + 1;
+            maqm_incrementahora <= 0;
+				end
+		end
 
 endmodule
