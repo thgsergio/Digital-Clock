bb5e117e44e771d8b80e7b65b4aff5475d6f0e0e
LuisEd83 <eduluis784@gmail.com>
2025-09-08 21:19:30 -0300

    Fix maq_h.sv

--- digital_clock/maq_h.sv
+++ digital_clock/maq_h.sv
@@ -7,7 +7,7 @@ module maq_h(
 	output logic [1:0] maqh_Msd
 );
 
-always_ff @(posedge maqh_clock, negedge maqh_reset) begin
+always_ff @(posedge maqh_clock or negedge maqh_reset) begin
 	if(!maqh_reset)
 		begin
 		maqh_Lsd <= 4'b0000;
@@ -15,12 +15,12 @@ always_ff @(posedge maqh_clock, negedge maqh_reset) begin
 		end
 	else if((maqh_enable) && (maqh_incremento))
 		begin
-        if ((maqh_Msd == 2'b10) && (maqh_Lsd == 4'b0100)) begin
+        if ((maqh_Msd == 2'b10) && (maqh_Lsd == 4'b0011)) begin
             maqh_Lsd <= 4'b0000;
             maqh_Msd <= 2'b00;
         end
-        else if (maqh_Lsd == 9) begin
-            maqh_Lsd <= 0;
+        else if (maqh_Lsd == 4'b1001) begin
+            maqh_Lsd <= 4'b0000;
             maqh_Msd <= maqh_Msd + 1;
         end
         else begin
