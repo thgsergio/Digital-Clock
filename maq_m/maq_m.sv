754c3377e4a8a5364b5f46ed88bdfc6ccde16a1e
Thiago Sergio <thiagolorax73@gmail.com>
2025-09-09 20:54:05 -0300

    fix: Avanços na solução do erro da hora

--- maq_m/maq_m.sv
+++ maq_m/maq_m.sv
@@ -12,44 +12,47 @@ always_ff @(posedge maqm_clock or negedge maqm_reset)
 begin
 	if(!maqm_reset)
 	begin
-		maqm_Lsd <= 4'b1001;
+		maqm_Lsd <= 4'b1000;
 		maqm_Msd <= 3'b101;
 		maqm_incrementahora <= 1'b0;
 	end
-	else if(maqm_enable && maqm_incremento)
+	else if(maqm_enable)
 	begin
-		if(maqm_Lsd >= 4'b1001) 
+		if(maqm_incremento)
 		begin
-			maqm_Lsd <= 4'b0000;
+			if(maqm_Lsd >= 4'b1001) 
+			begin
+				maqm_Lsd <= 4'b0000;
 
-			if(maqm_Msd >= 3'b101) 
+				if(maqm_Msd >= 3'b101) 
+				begin
+					maqm_Msd <= 3'b000;
+					maqm_incrementahora <= 1'b0;
+				end 
+				else 
+				begin
+					maqm_Msd <= maqm_Msd + 1'b1;
+					maqm_incrementahora <= 1'b0;
+				end
+			end
+			else if(maqm_Msd >= 3'b101)
 			begin
-				maqm_Msd <= 3'b000;
-				maqm_incrementahora <= 1'b0;
-			end 
+				if(maqm_Lsd >= 4'b1000 && maqm_Lsd <= 4'b1001)
+				begin
+					maqm_incrementahora <= 1'b1;
+				end
+				else 
+				begin
+					maqm_incrementahora <= 0;
+				end
+				maqm_Lsd <= maqm_Lsd + 1;
+			end
 			else 
-			begin
-				maqm_Msd <= maqm_Msd + 1'b1;
-				maqm_incrementahora <= 1'b0;
-			end
-		end
-		else if(maqm_Msd >= 3'b101)
-		begin
-			if(maqm_Lsd >= 4'b1000 && maqm_Lsd < 4'b1001)
-			begin
-				maqm_incrementahora <= 1'b1;
-			end
-			else 
-			begin
-				maqm_incrementahora <= 0;
-			end
-			maqm_Lsd <= maqm_Lsd + 1;
-		end
-		else 
 			begin
 				maqm_Lsd <= maqm_Lsd + 1;
 				maqm_incrementahora <= 0;
 			end
+		end
 	end
 end
 
